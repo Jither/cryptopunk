12b1a11c94b4cf7c05724a4a551c282c810eea28
Jimmi Thøgersen <jimmith@gmail.com>
2016-12-10 14:10:40 +0100

    Deoptimized HAVAL

--- src/scripts-es6/transforms/hashes/haval.js
+++ src/scripts-es6/transforms/hashes/haval.js
@@ -1,11 +1,81 @@
 import { MdHashTransform } from "./hash";
 import { int32sToBytesLE, bytesToInt32sLE } from "../../cryptopunk.utils";
-import { add } from "../../cryptopunk.bitarith";
+import { add, ror } from "../../cryptopunk.bitarith";
 
 const HAVAL_VERSION = 1;
 
 const PASSES_NAMES  = [3, 4, 5];
 const SIZE_NAMES  = [128, 160, 192, 224, 256];
+
+// Round constants for each pass
+// Starting from pass 2, each of these are 1024 bits of the fractional part of PI, starting from bit 256
+// (the first 256 bits are used for the initial state, see below)
+const ROUND_CONSTANTS = [
+	[
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0
+	],
+	[
+		0x452821e6, 0x38d01377, 0xbe5466cf, 0x34e90c6c, 0xc0ac29b7, 0xc97c50dd, 0x3f84d5b5, 0xb5470917, 
+		0x9216d5d9, 0x8979fb1b, 0xd1310ba6, 0x98dfb5ac, 0x2ffd72db, 0xd01adfb7, 0xb8e1afed, 0x6a267e96, 
+		0xba7c9045, 0xf12c7f99, 0x24a19947, 0xb3916cf7, 0x0801f2e2, 0x858efc16, 0x636920d8, 0x71574e69, 
+		0xa458fea3, 0xf4933d7e, 0x0d95748f, 0x728eb658, 0x718bcd58, 0x82154aee, 0x7b54a41d, 0xc25a59b5
+	],
+	[
+		0x9c30d539, 0x2af26013, 0xc5d1b023, 0x286085f0, 0xca417918, 0xb8db38ef, 0x8e79dcb0, 0x603a180e, 
+		0x6c9e0e8b, 0xb01e8a3e, 0xd71577c1, 0xbd314b27, 0x78af2fda, 0x55605c60, 0xe65525f3, 0xaa55ab94, 
+		0x57489862, 0x63e81440, 0x55ca396a, 0x2aab10b6, 0xb4cc5c34, 0x1141e8ce, 0xa15486af, 0x7c72e993, 
+		0xb3ee1411, 0x636fbc2a, 0x2ba9c55d, 0x741831f6, 0xce5c3e16, 0x9b87931e, 0xafd6ba33, 0x6c24cf5c
+	],
+	[
+		0x7a325381, 0x28958677, 0x3b8f4898, 0x6b4bb9af, 0xc4bfe81b, 0x66282193, 0x61d809cc, 0xfb21a991, 
+		0x487cac60, 0x5dec8032, 0xef845d5d, 0xe98575b1, 0xdc262302, 0xeb651b88, 0x23893e81, 0xd396acc5, 
+		0x0f6d6ff3, 0x83f44239, 0x2e0b4482, 0xa4842004, 0x69c8f04a, 0x9e1f9b5e, 0x21c66842, 0xf6e96c9a, 
+		0x670c9c61, 0xabd388f0, 0x6a51a0d2, 0xd8542f68, 0x960fa728, 0xab5133a3, 0x6eef0b6c, 0x137a3be4
+	],
+	[
+		0xba3bf050, 0x7efb2a98, 0xa1f1651d, 0x39af0176, 0x66ca593e, 0x82430e88, 0x8cee8619, 0x456f9fb4, 
+		0x7d84a5c3, 0x3b8b5ebe, 0xe06f75d8, 0x85c12073, 0x401a449f, 0x56c16aa6, 0x4ed3aa62, 0x363f7706, 
+		0x1bfedf72, 0x429b023d, 0x37d0d724, 0xd00a1248, 0xdb0fead3, 0x49f1c09b, 0x075372c9, 0x80991b7b, 
+		0x25d479d8, 0xf6e8def7, 0xe3fe501a, 0xb6794c3b, 0x976ce0bd, 0x04c006ba, 0xc1a94fb6, 0x409f60c4
+	]
+];
+
+// Word processing orders for each pass
+const ROUND_ORDERS = [
+	[
+		0, 1, 2, 3, 4, 5, 6, 7,
+		8, 9, 10, 11, 12, 13, 14, 15,
+		16, 17, 18, 19, 20, 21, 22, 23,
+		24, 25, 26, 27, 28, 29, 30, 31
+	],
+	[
+		5, 14, 26, 18, 11, 28, 7, 16,
+		0, 23, 20, 22, 1, 10, 4, 8,
+		30, 3, 21, 9, 17, 24, 29, 6,
+		19, 12, 15, 13, 2, 25, 31, 27
+	],
+	[
+		19, 9, 4, 20, 28, 17, 8, 22, 
+		29, 14, 25, 12, 24, 30, 16, 26, 
+		31, 15, 7, 3, 1, 0, 18, 27, 
+		13, 6, 21, 10, 23, 11, 5, 2
+	],
+	[
+		24, 4, 0, 14, 2, 7, 28, 23,
+		26, 6, 30, 20, 18, 25, 19, 3,
+		22, 11, 31, 21, 8, 27, 12, 9,
+		1, 29, 5, 15, 17, 10, 16, 13
+	],
+	[
+		27, 3, 21, 26, 17, 11, 20, 29, 
+		19, 0, 12, 7, 13, 8, 31, 10, 
+		5, 9, 14, 30, 18, 6, 28, 24,
+		2, 23, 16, 22, 4, 1, 25, 15
+	]
+];
 
 function f1(x6, x5, x4, x3, x2, x1, x0)
 {
@@ -32,78 +102,68 @@ function f5(x6, x5, x4, x3, x2, x1, x0)
 	return x0 & (x1 & x2 & x3 ^ (~x5)) ^ x1 & x4 ^ x2 & x5 ^ x3 & x6;
 }
 
+// These functions to permutation of the state before passing it on to
+// the 
 // camelcase is not very readable for these function names:
 /* eslint-disable camelcase */
-function ff1_3(x7, x6, x5, x4, x3, x2, x1, x0, w)
+function ff1_3(x)
 {
-	const t = f1(x1, x0, x3, x5, x6, x2, x4);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w);
+	return f1(x[1], x[0], x[3], x[5], x[6], x[2], x[4]);
 }
 
-function ff1_4(x7, x6, x5, x4, x3, x2, x1, x0, w)
+function ff1_4(x)
 {
-	const t = f1(x2, x6, x1, x4, x5, x3, x0);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w);
+	return f1(x[2], x[6], x[1], x[4], x[5], x[3], x[0]);
 }
 
-function ff1_5(x7, x6, x5, x4, x3, x2, x1, x0, w)
+function ff1_5(x)
 {
-	const t = f1(x3, x4, x1, x0, x5, x2, x6);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w);
+	return f1(x[3], x[4], x[1], x[0], x[5], x[2], x[6]);
 }
 
-function ff2_3(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff2_3(x)
 {
-	const t = f2(x4, x2, x1, x0, x5, x3, x6);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w, c);
+	return f2(x[4], x[2], x[1], x[0], x[5], x[3], x[6]);
 }
 
-function ff2_4(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff2_4(x)
 {
-	const t = f2(x3, x5, x2, x0, x1, x6, x4);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w, c);
+	return f2(x[3], x[5], x[2], x[0], x[1], x[6], x[4]);
 }
 
-function ff2_5(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff2_5(x)
 {
-	const t = f2(x6, x2, x1, x0, x3, x4, x5);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w, c);
+	return f2(x[6], x[2], x[1], x[0], x[3], x[4], x[5]);
 }
 
-function ff3_3(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff3_3(x)
 {
-	const t = f3(x6, x1, x2, x3, x4, x5, x0);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w, c);
+	return f3(x[6], x[1], x[2], x[3], x[4], x[5], x[0]);
 }
 
-function ff3_4(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff3_4(x)
 {
-	const t = f3(x1, x4, x3, x6, x0, x2, x5);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w, c);
+	return f3(x[1], x[4], x[3], x[6], x[0], x[2], x[5]);
 }
 
-function ff3_5(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff3_5(x)
 {
-	const t = f3(x2, x6, x0, x4, x3, x1, x5);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w, c);
+	return f3(x[2], x[6], x[0], x[4], x[3], x[1], x[5]);
 }
 
-function ff4_4(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff4_4(x)
 {
-	const t = f4(x6, x4, x0, x5, x2, x1, x3);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w,  c);
+	return f4(x[6], x[4], x[0], x[5], x[2], x[1], x[3]);
 }
 
-function ff4_5(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff4_5(x)
 {
-	const t = f4(x1, x5, x3, x2, x0, x4, x6);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w,  c);
+	return f4(x[1], x[5], x[3], x[2], x[0], x[4], x[6]);
 }
 
-function ff5_5(x7, x6, x5, x4, x3, x2, x1, x0, w, c)
+function ff5_5(x)
 {
-	const t = f5(x2, x5, x0, x6, x4, x3, x1);
-	return add((t >>> 7 | t << 25) + (x7 >>> 11 | x7 << 21), w, c);
+	return f5(x[2], x[5], x[0], x[6], x[4], x[3], x[1]);
 }
 /* eslint-enable camelcase */
 
@@ -132,6 +192,7 @@ class HavalTransform extends MdHashTransform
 
 	transform(bytes)
 	{
+		// Initial state: first 256 bits of PI:
 		const state = [
 			0x243f6a88,
 			0x85a308d3,
@@ -144,27 +205,28 @@ class HavalTransform extends MdHashTransform
 		];
 
 		const context = { state };
+		const ff = context.ff = [];
 
 		/* eslint-disable camelcase */
 		switch (this.options.passes)
 		{
 			case 3:
-				context.ff1 = ff1_3;
-				context.ff2 = ff2_3;
-				context.ff3 = ff3_3;
+				ff[0] = ff1_3;
+				ff[1] = ff2_3;
+				ff[2] = ff3_3;
 				break;
 			case 4:
-				context.ff1 = ff1_4;
-				context.ff2 = ff2_4;
-				context.ff3 = ff3_4;
-				context.ff4 = ff4_4;
+				ff[0] = ff1_4;
+				ff[1] = ff2_4;
+				ff[2] = ff3_4;
+				ff[3] = ff4_4;
 				break;
 			default:
-				context.ff1 = ff1_5;
-				context.ff2 = ff2_5;
-				context.ff3 = ff3_5;
-				context.ff4 = ff4_5;
-				context.ff5 = ff5_5;
+				ff[0] = ff1_5;
+				ff[1] = ff2_5;
+				ff[2] = ff3_5;
+				ff[3] = ff4_5;
+				ff[4] = ff5_5;
 				break;
 		}
 		/* eslint-enable camelcase */
@@ -176,217 +238,42 @@ class HavalTransform extends MdHashTransform
 
 	transformBlock(block, context)
 	{
-		const x = bytesToInt32sLE(block);
+		const b = bytesToInt32sLE(block);
+		const state = context.state;
 
-		const
-			state = context.state,
-			ff1 = context.ff1, 
-			ff2 = context.ff2, 
-			ff3 = context.ff3, 
-			ff4 = context.ff4,
-			ff5 = context.ff5;
+		const x = state.concat();
 
-		let [t0, t1, t2, t3, t4, t5, t6, t7] = state;
-
-		// TODO: Roll passes into loops
-
-		// Pass 1
-		t7 = ff1(t7, t6, t5, t4, t3, t2, t1, t0, x[ 0]);
-		t6 = ff1(t6, t5, t4, t3, t2, t1, t0, t7, x[ 1]);
-		t5 = ff1(t5, t4, t3, t2, t1, t0, t7, t6, x[ 2]);
-		t4 = ff1(t4, t3, t2, t1, t0, t7, t6, t5, x[ 3]);
-		t3 = ff1(t3, t2, t1, t0, t7, t6, t5, t4, x[ 4]);
-		t2 = ff1(t2, t1, t0, t7, t6, t5, t4, t3, x[ 5]);
-		t1 = ff1(t1, t0, t7, t6, t5, t4, t3, t2, x[ 6]);
-		t0 = ff1(t0, t7, t6, t5, t4, t3, t2, t1, x[ 7]);
-
-		t7 = ff1(t7, t6, t5, t4, t3, t2, t1, t0, x[ 8]);
-		t6 = ff1(t6, t5, t4, t3, t2, t1, t0, t7, x[ 9]);
-		t5 = ff1(t5, t4, t3, t2, t1, t0, t7, t6, x[10]);
-		t4 = ff1(t4, t3, t2, t1, t0, t7, t6, t5, x[11]);
-		t3 = ff1(t3, t2, t1, t0, t7, t6, t5, t4, x[12]);
-		t2 = ff1(t2, t1, t0, t7, t6, t5, t4, t3, x[13]);
-		t1 = ff1(t1, t0, t7, t6, t5, t4, t3, t2, x[14]);
-		t0 = ff1(t0, t7, t6, t5, t4, t3, t2, t1, x[15]);
-
-		t7 = ff1(t7, t6, t5, t4, t3, t2, t1, t0, x[16]);
-		t6 = ff1(t6, t5, t4, t3, t2, t1, t0, t7, x[17]);
-		t5 = ff1(t5, t4, t3, t2, t1, t0, t7, t6, x[18]);
-		t4 = ff1(t4, t3, t2, t1, t0, t7, t6, t5, x[19]);
-		t3 = ff1(t3, t2, t1, t0, t7, t6, t5, t4, x[20]);
-		t2 = ff1(t2, t1, t0, t7, t6, t5, t4, t3, x[21]);
-		t1 = ff1(t1, t0, t7, t6, t5, t4, t3, t2, x[22]);
-		t0 = ff1(t0, t7, t6, t5, t4, t3, t2, t1, x[23]);
-
-		t7 = ff1(t7, t6, t5, t4, t3, t2, t1, t0, x[24]);
-		t6 = ff1(t6, t5, t4, t3, t2, t1, t0, t7, x[25]);
-		t5 = ff1(t5, t4, t3, t2, t1, t0, t7, t6, x[26]);
-		t4 = ff1(t4, t3, t2, t1, t0, t7, t6, t5, x[27]);
-		t3 = ff1(t3, t2, t1, t0, t7, t6, t5, t4, x[28]);
-		t2 = ff1(t2, t1, t0, t7, t6, t5, t4, t3, x[29]);
-		t1 = ff1(t1, t0, t7, t6, t5, t4, t3, t2, x[30]);
-		t0 = ff1(t0, t7, t6, t5, t4, t3, t2, t1, x[31]);
-
-		// Pass 2
-		t7 = ff2(t7, t6, t5, t4, t3, t2, t1, t0, x[ 5], 0x452821e6);
-		t6 = ff2(t6, t5, t4, t3, t2, t1, t0, t7, x[14], 0x38d01377);
-		t5 = ff2(t5, t4, t3, t2, t1, t0, t7, t6, x[26], 0xbe5466cf);
-		t4 = ff2(t4, t3, t2, t1, t0, t7, t6, t5, x[18], 0x34e90c6c);
-		t3 = ff2(t3, t2, t1, t0, t7, t6, t5, t4, x[11], 0xc0ac29b7);
-		t2 = ff2(t2, t1, t0, t7, t6, t5, t4, t3, x[28], 0xc97c50dd);
-		t1 = ff2(t1, t0, t7, t6, t5, t4, t3, t2, x[ 7], 0x3f84d5b5);
-		t0 = ff2(t0, t7, t6, t5, t4, t3, t2, t1, x[16], 0xb5470917);
-
-		t7 = ff2(t7, t6, t5, t4, t3, t2, t1, t0, x[ 0], 0x9216d5d9);
-		t6 = ff2(t6, t5, t4, t3, t2, t1, t0, t7, x[23], 0x8979fb1b);
-		t5 = ff2(t5, t4, t3, t2, t1, t0, t7, t6, x[20], 0xd1310ba6);
-		t4 = ff2(t4, t3, t2, t1, t0, t7, t6, t5, x[22], 0x98dfb5ac);
-		t3 = ff2(t3, t2, t1, t0, t7, t6, t5, t4, x[ 1], 0x2ffd72db);
-		t2 = ff2(t2, t1, t0, t7, t6, t5, t4, t3, x[10], 0xd01adfb7);
-		t1 = ff2(t1, t0, t7, t6, t5, t4, t3, t2, x[ 4], 0xb8e1afed);
-		t0 = ff2(t0, t7, t6, t5, t4, t3, t2, t1, x[ 8], 0x6a267e96);
-
-		t7 = ff2(t7, t6, t5, t4, t3, t2, t1, t0, x[30], 0xba7c9045);
-		t6 = ff2(t6, t5, t4, t3, t2, t1, t0, t7, x[ 3], 0xf12c7f99);
-		t5 = ff2(t5, t4, t3, t2, t1, t0, t7, t6, x[21], 0x24a19947);
-		t4 = ff2(t4, t3, t2, t1, t0, t7, t6, t5, x[ 9], 0xb3916cf7);
-		t3 = ff2(t3, t2, t1, t0, t7, t6, t5, t4, x[17], 0x0801f2e2);
-		t2 = ff2(t2, t1, t0, t7, t6, t5, t4, t3, x[24], 0x858efc16);
-		t1 = ff2(t1, t0, t7, t6, t5, t4, t3, t2, x[29], 0x636920d8);
-		t0 = ff2(t0, t7, t6, t5, t4, t3, t2, t1, x[ 6], 0x71574e69);
-
-		t7 = ff2(t7, t6, t5, t4, t3, t2, t1, t0, x[19], 0xa458fea3);
-		t6 = ff2(t6, t5, t4, t3, t2, t1, t0, t7, x[12], 0xf4933d7e);
-		t5 = ff2(t5, t4, t3, t2, t1, t0, t7, t6, x[15], 0x0d95748f);
-		t4 = ff2(t4, t3, t2, t1, t0, t7, t6, t5, x[13], 0x728eb658);
-		t3 = ff2(t3, t2, t1, t0, t7, t6, t5, t4, x[ 2], 0x718bcd58);
-		t2 = ff2(t2, t1, t0, t7, t6, t5, t4, t3, x[25], 0x82154aee);
-		t1 = ff2(t1, t0, t7, t6, t5, t4, t3, t2, x[31], 0x7b54a41d);
-		t0 = ff2(t0, t7, t6, t5, t4, t3, t2, t1, x[27], 0xc25a59b5);
-
-		// Pass 3
-		t7 = ff3(t7, t6, t5, t4, t3, t2, t1, t0, x[19], 0x9c30d539);
-		t6 = ff3(t6, t5, t4, t3, t2, t1, t0, t7, x[ 9], 0x2af26013);
-		t5 = ff3(t5, t4, t3, t2, t1, t0, t7, t6, x[ 4], 0xc5d1b023);
-		t4 = ff3(t4, t3, t2, t1, t0, t7, t6, t5, x[20], 0x286085f0);
-		t3 = ff3(t3, t2, t1, t0, t7, t6, t5, t4, x[28], 0xca417918);
-		t2 = ff3(t2, t1, t0, t7, t6, t5, t4, t3, x[17], 0xb8db38ef);
-		t1 = ff3(t1, t0, t7, t6, t5, t4, t3, t2, x[ 8], 0x8e79dcb0);
-		t0 = ff3(t0, t7, t6, t5, t4, t3, t2, t1, x[22], 0x603a180e);
-
-		t7 = ff3(t7, t6, t5, t4, t3, t2, t1, t0, x[29], 0x6c9e0e8b);
-		t6 = ff3(t6, t5, t4, t3, t2, t1, t0, t7, x[14], 0xb01e8a3e);
-		t5 = ff3(t5, t4, t3, t2, t1, t0, t7, t6, x[25], 0xd71577c1);
-		t4 = ff3(t4, t3, t2, t1, t0, t7, t6, t5, x[12], 0xbd314b27);
-		t3 = ff3(t3, t2, t1, t0, t7, t6, t5, t4, x[24], 0x78af2fda);
-		t2 = ff3(t2, t1, t0, t7, t6, t5, t4, t3, x[30], 0x55605c60);
-		t1 = ff3(t1, t0, t7, t6, t5, t4, t3, t2, x[16], 0xe65525f3);
-		t0 = ff3(t0, t7, t6, t5, t4, t3, t2, t1, x[26], 0xaa55ab94);
-
-		t7 = ff3(t7, t6, t5, t4, t3, t2, t1, t0, x[31], 0x57489862);
-		t6 = ff3(t6, t5, t4, t3, t2, t1, t0, t7, x[15], 0x63e81440);
-		t5 = ff3(t5, t4, t3, t2, t1, t0, t7, t6, x[ 7], 0x55ca396a);
-		t4 = ff3(t4, t3, t2, t1, t0, t7, t6, t5, x[ 3], 0x2aab10b6);
-		t3 = ff3(t3, t2, t1, t0, t7, t6, t5, t4, x[ 1], 0xb4cc5c34);
-		t2 = ff3(t2, t1, t0, t7, t6, t5, t4, t3, x[ 0], 0x1141e8ce);
-		t1 = ff3(t1, t0, t7, t6, t5, t4, t3, t2, x[18], 0xa15486af);
-		t0 = ff3(t0, t7, t6, t5, t4, t3, t2, t1, x[27], 0x7c72e993);
-
-		t7 = ff3(t7, t6, t5, t4, t3, t2, t1, t0, x[13], 0xb3ee1411);
-		t6 = ff3(t6, t5, t4, t3, t2, t1, t0, t7, x[ 6], 0x636fbc2a);
-		t5 = ff3(t5, t4, t3, t2, t1, t0, t7, t6, x[21], 0x2ba9c55d);
-		t4 = ff3(t4, t3, t2, t1, t0, t7, t6, t5, x[10], 0x741831f6);
-		t3 = ff3(t3, t2, t1, t0, t7, t6, t5, t4, x[23], 0xce5c3e16);
-		t2 = ff3(t2, t1, t0, t7, t6, t5, t4, t3, x[11], 0x9b87931e);
-		t1 = ff3(t1, t0, t7, t6, t5, t4, t3, t2, x[ 5], 0xafd6ba33);
-		t0 = ff3(t0, t7, t6, t5, t4, t3, t2, t1, x[ 2], 0x6c24cf5c);
-
-		if (this.options.passes >= 4)
+		for (let pass = 0; pass < this.options.passes; pass++)
 		{
-			t7 = ff4(t7, t6, t5, t4, t3, t2, t1, t0, x[24], 0x7a325381);
-			t6 = ff4(t6, t5, t4, t3, t2, t1, t0, t7, x[ 4], 0x28958677);
-			t5 = ff4(t5, t4, t3, t2, t1, t0, t7, t6, x[ 0], 0x3b8f4898);
-			t4 = ff4(t4, t3, t2, t1, t0, t7, t6, t5, x[14], 0x6b4bb9af);
-			t3 = ff4(t3, t2, t1, t0, t7, t6, t5, t4, x[ 2], 0xc4bfe81b);
-			t2 = ff4(t2, t1, t0, t7, t6, t5, t4, t3, x[ 7], 0x66282193);
-			t1 = ff4(t1, t0, t7, t6, t5, t4, t3, t2, x[28], 0x61d809cc);
-			t0 = ff4(t0, t7, t6, t5, t4, t3, t2, t1, x[23], 0xfb21a991);
+			const
+				ff = context.ff[pass],
+				orders = ROUND_ORDERS[pass],
+				rcon = ROUND_CONSTANTS[pass];
 
-			t7 = ff4(t7, t6, t5, t4, t3, t2, t1, t0, x[26], 0x487cac60);
-			t6 = ff4(t6, t5, t4, t3, t2, t1, t0, t7, x[ 6], 0x5dec8032);
-			t5 = ff4(t5, t4, t3, t2, t1, t0, t7, t6, x[30], 0xef845d5d);
-			t4 = ff4(t4, t3, t2, t1, t0, t7, t6, t5, x[20], 0xe98575b1);
-			t3 = ff4(t3, t2, t1, t0, t7, t6, t5, t4, x[18], 0xdc262302);
-			t2 = ff4(t2, t1, t0, t7, t6, t5, t4, t3, x[25], 0xeb651b88);
-			t1 = ff4(t1, t0, t7, t6, t5, t4, t3, t2, x[19], 0x23893e81);
-			t0 = ff4(t0, t7, t6, t5, t4, t3, t2, t1, x[ 3], 0xd396acc5);
-
-			t7 = ff4(t7, t6, t5, t4, t3, t2, t1, t0, x[22], 0x0f6d6ff3);
-			t6 = ff4(t6, t5, t4, t3, t2, t1, t0, t7, x[11], 0x83f44239);
-			t5 = ff4(t5, t4, t3, t2, t1, t0, t7, t6, x[31], 0x2e0b4482);
-			t4 = ff4(t4, t3, t2, t1, t0, t7, t6, t5, x[21], 0xa4842004);
-			t3 = ff4(t3, t2, t1, t0, t7, t6, t5, t4, x[ 8], 0x69c8f04a);
-			t2 = ff4(t2, t1, t0, t7, t6, t5, t4, t3, x[27], 0x9e1f9b5e);
-			t1 = ff4(t1, t0, t7, t6, t5, t4, t3, t2, x[12], 0x21c66842);
-			t0 = ff4(t0, t7, t6, t5, t4, t3, t2, t1, x[ 9], 0xf6e96c9a);
-
-			t7 = ff4(t7, t6, t5, t4, t3, t2, t1, t0, x[ 1], 0x670c9c61);
-			t6 = ff4(t6, t5, t4, t3, t2, t1, t0, t7, x[29], 0xabd388f0);
-			t5 = ff4(t5, t4, t3, t2, t1, t0, t7, t6, x[ 5], 0x6a51a0d2);
-			t4 = ff4(t4, t3, t2, t1, t0, t7, t6, t5, x[15], 0xd8542f68);
-			t3 = ff4(t3, t2, t1, t0, t7, t6, t5, t4, x[17], 0x960fa728);
-			t2 = ff4(t2, t1, t0, t7, t6, t5, t4, t3, x[10], 0xab5133a3);
-			t1 = ff4(t1, t0, t7, t6, t5, t4, t3, t2, x[16], 0x6eef0b6c);
-			t0 = ff4(t0, t7, t6, t5, t4, t3, t2, t1, x[13], 0x137a3be4);
+			for (let round = 0; round < 32; round++)
+			{
+				const t = ff(x);
+				// Shift state 1 word left:
+				const x7 = x[7];
+				x[7] = x[6];
+				x[6] = x[5];
+				x[5] = x[4];
+				x[4] = x[3];
+				x[3] = x[2];
+				x[2] = x[1];
+				x[1] = x[0];
+				x[0] = add(ror(t, 7), ror(x7, 11), b[orders[round]], rcon[round]);
+			}
 		}
 
-		if (this.options.passes >= 5)
-		{
-			t7 = ff5(t7, t6, t5, t4, t3, t2, t1, t0, x[27], 0xba3bf050);
-			t6 = ff5(t6, t5, t4, t3, t2, t1, t0, t7, x[ 3], 0x7efb2a98);
-			t5 = ff5(t5, t4, t3, t2, t1, t0, t7, t6, x[21], 0xa1f1651d);
-			t4 = ff5(t4, t3, t2, t1, t0, t7, t6, t5, x[26], 0x39af0176);
-			t3 = ff5(t3, t2, t1, t0, t7, t6, t5, t4, x[17], 0x66ca593e);
-			t2 = ff5(t2, t1, t0, t7, t6, t5, t4, t3, x[11], 0x82430e88);
-			t1 = ff5(t1, t0, t7, t6, t5, t4, t3, t2, x[20], 0x8cee8619);
-			t0 = ff5(t0, t7, t6, t5, t4, t3, t2, t1, x[29], 0x456f9fb4);
-
-			t7 = ff5(t7, t6, t5, t4, t3, t2, t1, t0, x[19], 0x7d84a5c3);
-			t6 = ff5(t6, t5, t4, t3, t2, t1, t0, t7, x[ 0], 0x3b8b5ebe);
-			t5 = ff5(t5, t4, t3, t2, t1, t0, t7, t6, x[12], 0xe06f75d8);
-			t4 = ff5(t4, t3, t2, t1, t0, t7, t6, t5, x[ 7], 0x85c12073);
-			t3 = ff5(t3, t2, t1, t0, t7, t6, t5, t4, x[13], 0x401a449f);
-			t2 = ff5(t2, t1, t0, t7, t6, t5, t4, t3, x[ 8], 0x56c16aa6);
-			t1 = ff5(t1, t0, t7, t6, t5, t4, t3, t2, x[31], 0x4ed3aa62);
-			t0 = ff5(t0, t7, t6, t5, t4, t3, t2, t1, x[10], 0x363f7706);
-
-			t7 = ff5(t7, t6, t5, t4, t3, t2, t1, t0, x[ 5], 0x1bfedf72);
-			t6 = ff5(t6, t5, t4, t3, t2, t1, t0, t7, x[ 9], 0x429b023d);
-			t5 = ff5(t5, t4, t3, t2, t1, t0, t7, t6, x[14], 0x37d0d724);
-			t4 = ff5(t4, t3, t2, t1, t0, t7, t6, t5, x[30], 0xd00a1248);
-			t3 = ff5(t3, t2, t1, t0, t7, t6, t5, t4, x[18], 0xdb0fead3);
-			t2 = ff5(t2, t1, t0, t7, t6, t5, t4, t3, x[ 6], 0x49f1c09b);
-			t1 = ff5(t1, t0, t7, t6, t5, t4, t3, t2, x[28], 0x075372c9);
-			t0 = ff5(t0, t7, t6, t5, t4, t3, t2, t1, x[24], 0x80991b7b);
-
-			t7 = ff5(t7, t6, t5, t4, t3, t2, t1, t0, x[ 2], 0x25d479d8);
-			t6 = ff5(t6, t5, t4, t3, t2, t1, t0, t7, x[23], 0xf6e8def7);
-			t5 = ff5(t5, t4, t3, t2, t1, t0, t7, t6, x[16], 0xe3fe501a);
-			t4 = ff5(t4, t3, t2, t1, t0, t7, t6, t5, x[22], 0xb6794c3b);
-			t3 = ff5(t3, t2, t1, t0, t7, t6, t5, t4, x[ 4], 0x976ce0bd);
-			t2 = ff5(t2, t1, t0, t7, t6, t5, t4, t3, x[ 1], 0x04c006ba);
-			t1 = ff5(t1, t0, t7, t6, t5, t4, t3, t2, x[25], 0xc1a94fb6);
-			t0 = ff5(t0, t7, t6, t5, t4, t3, t2, t1, x[15], 0x409f60c4);
-		}
-
-		state[0] = add(state[0], t0);
-		state[1] = add(state[1], t1);
-		state[2] = add(state[2], t2);
-		state[3] = add(state[3], t3);
-		state[4] = add(state[4], t4);
-		state[5] = add(state[5], t5);
-		state[6] = add(state[6], t6);
-		state[7] = add(state[7], t7);
+		state[0] = add(state[0], x[0]);
+		state[1] = add(state[1], x[1]);
+		state[2] = add(state[2], x[2]);
+		state[3] = add(state[3], x[3]);
+		state[4] = add(state[4], x[4]);
+		state[5] = add(state[5], x[5]);
+		state[6] = add(state[6], x[6]);
+		state[7] = add(state[7], x[7]);
 	}
 
 	tailor(state, size)
